ffe50a6b9aa291ace6280d6199d44da58cc458f7
Jacob <jakeguss@gmail.com>
2019-12-01 21:30:27 -0700

    added client folder

---
+++ client/src/AddChar/AddChar.js
@@ -0,0 +1,241 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import {Grid} from '@material-ui/core';
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import marvelLogo from'./marvel-logo.png';
+import DCLogo from'./DC-logo.png';
+import imageLogo from './image-logo.png';
+import valiantLogo from './valiant-logo.jpeg';
+import darkHorseLogo from './dark-horse-logo.jpg';
+import noImageFound from "./image-default.jpg";
+import IDWLogo from './IDW-logo.png';
+import './AddChar.css';
+
+
+class AddChar extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        apiURL: '',
+        name: '',
+        civName: '',
+        firstDebut: '',
+        alignment: 'Hero',
+        teams: [],
+        abilities: [],
+        company: '',
+        imageURL:'',
+        desc: '',
+        formSubmitted: false,
+        readyToSubmit: false,
+        attemptMessage: ''
+      }
+  
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.postCharacter = this.postCharacter.bind(this);
+  
+    }
+    
+    postCharacter() {
+        axios.post('/api/chars/create',{
+            name: this.state.name,
+            civName: this.state.civName,
+            firstDebut: this.state.firstDebut,
+            alignment: this.state.alignment,
+            teams: this.state.teams,
+            abilities: this.state.abilities,
+            company: this.state.company,
+            imageURL: this.state.imageURL,
+            desc: this.state.desc
+
+        }).then((response) => {
+            // handle success
+            console.log(response);
+        })
+        .catch( (error) => {
+            // handle error
+            console.log(error);
+        });            
+    }
+    
+    //handle the submit using this.
+    handleSubmit(event) {
+      event.preventDefault();
+      this.postCharacter();
+      this.setState({formSubmitted: true});
+    }
+   
+    render() {
+
+        function cardStylePicker(company) {
+
+            switch(company.toUpperCase()) {
+                case "MARVEL":
+                    return "custom-card marvel";
+                    break;
+                case "DC":
+                    return "custom-card dc";
+                    break;
+                default:
+                    return "custom-card indy";
+            }
+        }
+    
+        function logoPicker(company) {
+    
+            switch(company.toUpperCase()) {
+                case "MARVEL":
+                    return marvelLogo;
+                    break;
+                case "DC":
+                    return DCLogo;
+                    break;
+                case "IMAGE":
+                    return imageLogo;
+                    break;
+                case "VALIANT":
+                    return valiantLogo;
+                    break;
+                case "DARK HORSE":
+                    return darkHorseLogo;
+                    break;
+                case "IDW":
+                    return IDWLogo;
+                    break;   
+                default:
+                    return "";
+            }
+        }
+
+        let loggedIn = localStorage.getItem("authorized");
+        console.log(`the loggedIn is ${loggedIn}`);
+
+        if (this.state.formSubmitted === true) {
+            return (<Redirect to="/upload/success" />)
+        }
+        else if (loggedIn === 'true') {
+            return (
+                <div className="addchar" style={{padding: 12}}>
+                    <Grid container spacing={24} justify="flex-end">
+                        <Grid item lg={1}>
+                            <Button variant="contained" color="primary"><Link to="/">SEARCH</Link></Button>
+                        </Grid>
+                        <Grid item lg={1}>
+                            <Button variant="contained" color="primary" onClick={this.props.handleLogout}><Link to="/">LOG OUT</Link></Button>
+                        </Grid>
+                    </Grid>
+                    <h1 className="addchar-header">Character Upload</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-container ">
+                            <div className="form-item">
+                                <p>Name</p>
+                                <input type="text" value={this.state.name} onChange={ event => this.setState({ name: event.target.value })} />   
+                            </div>
+                            <div className="form-item">
+                                <p>Civilian Name</p>
+                                <input type="text" value={this.state.civName} onChange={ event => this.setState({ civName: event.target.value })} />
+                            </div>
+                            
+                            <div className="form-item">
+                                <p>First Debut</p>
+                                <input type="text" value={this.state.firstDebut} onChange={ event => this.setState({ firstDebut: event.target.value })} />
+                            </div>
+                            
+                            <div className="form-item">
+                                <p>Alignment</p>
+                                <select onChange={event => this.setState({ alignment: event.target.value })}>
+                                    <option value="Hero">Hero</option>
+                                    <option value="Villain">Villain</option>
+                                    <option value="Antihero">Antihero</option>
+                                </select>
+                            </div>
+                            
+                            <div className="form-item">
+                                <p>Abilities</p>
+                                <textarea type="text" value={this.state.abilities} onChange={ event => this.setState({ abilities: event.target.value.split(',') })} />
+                            </div>
+                            
+                            <div className="form-item">
+                                <p>Teams</p>
+                                <textarea type="text" value={this.state.teams} onChange={ event => this.setState({ teams: event.target.value.split(',') })} />
+                            </div>
+                            
+                            <div className="form-item">
+                                <p>Biography</p>
+                                <textarea type="text" value={this.state.desc} onChange={ event => this.setState({ desc: event.target.value })} />
+                            </div>
+                            
+                            <div className="form-item">
+                                <p>Company</p>
+                                <input type="text" value={this.state.company} onChange={ event => this.setState({ company: event.target.value })} />
+                            </div>
+                            
+                            <div className="form-item">
+                                <p>Character Picture</p>
+                                <input type="text" value={this.state.imageURL} onChange={ event => this.setState({ imageURL: event.target.value })} />
+                            </div>
+                            
+                            <div className="form-item submit-container">
+                                <input type="submit" value="SUBMIT" className="add-char-submit"/>
+                            </div>
+
+                        </div>
+                    </form>   
+                    <div className={cardStylePicker(this.state.company)}>
+                        <div className="cardTop">
+                            <div className="row">
+                                <div className="card-container image-container">
+                                    <img className="charImage" src={(this.state.imageURL) ? this.state.imageURL : noImageFound} />
+                                </div>
+                                <div className="column card-container">
+                                    <h1>{this.state.name ? this.state.name.toUpperCase() : "CHARACTER NAME"}</h1>
+                                    <div className="cardTop2">
+                                        <div className="text-center card-container">
+                                            <b>Name: </b>
+                                            <p>{this.state.civName}</p>
+                                        </div>
+                                        <div className="text-center card-container">
+                                            <b>Debut: </b>
+                                            <p>{this.state.firstDebut}</p>
+                                        </div>
+                                    </div>                                   
+                                    <div className="row">
+                                        <div className="column card-container abilities">
+                                            <h2 className="text-center">ABILITIES</h2>
+                                            <ul className="abilities-list">
+                                                {this.state.abilities.map(ability => {
+                                                    return (<li className="text-align-left word-wrap">{ability}</li>)
+                                                })}
+                                            </ul>
+                                        </div>
+                                        <div className="column card-container teams">
+                                            <h2 className="text-center">TEAMS</h2>
+                                            <ul className="team-list">
+                                            {this.state.teams.map(team => {
+                                                return (<li className="text-align-left word-wrap">{team}</li>)
+                                            })}
+                                            </ul>
+                                        </div >
+                                    </div>
+                                </div>
+                            </div>                                 
+                            <div className="cardBottom row">
+                                <div className="card-container text-center">
+                                    <p className="description"><i>{this.state.desc ? this.state.desc : "This is where the character's description or biograpy will go. Put all pertinent information here."}</i></p>
+                                </div>
+                                <div className="logo-bar card-container">
+                                    {this.state.company ?
+                                    <img className="company-logo" src={logoPicker(this.state.company)} alt="Logo"/> : <div></div>}       
+                                </div>              
+                            </div> 
+                        </div>                           
+                    </div>                                 
+                </div>              
+            );
+        } else {
+            return (<Redirect to="/" />) }  
+    }
+}
+export default AddChar;
--- server.js
+++ server.js
@@ -4,14 +4,247 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//Mongoose stuff
+mongoose.connect('mongodb+srv://excelsiorAdmin:Supermodel200!@excelsiorcluster-zakfd.mongodb.net/test?retryWrites=true', { useUnifiedTopology: true, useNewUrlParser: true, dbName: 'excelsiorDB'});
+const dbConnection = mongoose.connection;
+
+dbConnection.on('error', console.error.bind(console, 'connection error:'));
+dbConnection.once('open', function() {
+    console.log('connected to the database');
+
+    let charSchema = new mongoose.Schema({
+        imageURL: String,
+        company: String,
+        name: String,
+        civName: String,
+        alignment: String,
+        firstDebut: String,
+        abilities: [String],
+        teams: [String],
+        desc: String
+    });
+
+    let Char = mongoose.model('Char', charSchema, "excelsiorCollection");
+
+    //cors middleware
+    app.use(cors())
+
+    //bodyparser middleware
+    app.use(bodyParser.json()); // support json encoded bodies
+    app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+    
+    // the __dirname is the current directory from where the script is running
+    app.use(favicon(__dirname + '/build/favicon.ico'));
+    app.use(express.static(__dirname));
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    //ping pong
+    app.get('/ping', function (req, res) {
+      return res.send('pong');
+     });
+
+    //index.html 
+     app.get('/*', function (req, res) {
+       res.sendFile(path.join(__dirname, 'build', 'index.html'));
+     });
+
+    //root
+    app.get('/api', (req, res, next) => res.send('Welcome to the API!'));
+
+    //get all characters
+    app.get('/api/chars/all', (req, res, next) => {
+        console.log('getting all characters');
+        Char.find(function (err, chars) {
+            if (err) {
+                res.status(404).send(err);
+                console.log('there was an error');
+              };
+              console.log(chars);
+              res.send(chars);
+        });
+    });
+
+    //get heroes or villains
+    app.get('/api/chars/alignment/:alignment', (req, res, next) => {
+
+        if (req.params.alignment.toUpperCase() === "HERO") {
+            Char.find({alignment: "Hero"}, function (err, chars) {
+                if (err) {
+                    res.status(404).send(err);
+                };
+                console.log('getting all the heroes.')
+                res.send(chars);
+            });
+        }
+        else if (req.params.alignment.toUpperCase() === "VILLAIN") {
+            Char.find({alignment: "Villain"}, function (err, chars) {
+                if (err) {
+                    res.status(404).send(err);
+                };
+                console.log('getting all the villains.')
+                res.send(chars);
+            });
+        }
+        else if (req.params.alignment.toUpperCase() === "ANTIHERO") {
+            Char.find({alignment: "Antihero"}, function (err, chars) {
+                if (err) {
+                    res.status(404).send(err);
+                };
+                console.log('getting all the Antiheroes.')
+                res.send(chars);
+            });
+        }
+        else {
+            res.status(404).send();
+        }
+        
+    });
+
+    //get character by name
+    app.get('/api/chars/name/:name', (req, res, next) => {
+
+        Char.find({name: req.params.name}, function (err, chars) {
+            if (err) {
+                res.status(404).send(err);
+            };
+            console.log('getting the character by name.')
+            res.send(chars);
+        });
+    });
+
+    //get characters by team
+    app.get('/api/chars/teamsearch', (req, res, next) => {
+
+        let teamString = JSON.parse(req.query.teams).toString();
+
+        Char.find({teams: teamString}, function (err, chars) {
+            if (err) {
+                res.status(404).send(err);
+            };
+            res.send(chars);
+        });
+    });
+
+
+    //search characters with full query params
+    app.get('/api/chars/search', (req, res, next) => {
+        
+        let paramsObject = {};
+
+        if (req.query.company) {
+            paramsObject.company = { "$regex": req.query.company, "$options": "i" };
+        }
+        if (req.query.name) {
+            paramsObject.name = { "$regex": req.query.name, "$options": "i" };
+        }
+        if (req.query.alignment) {
+            paramsObject.alignment = { "$regex": req.query.alignment, "$options": "i" }
+        }
+        if (req.query.firstdebut) { 
+            paramsObject.firstDebut = { "$regex": req.query.firstdebut, "$options": "i" };
+        }
+        if (req.query.teams) {
+            paramsObject.teams = { "$regex": JSON.parse(req.query.teams).toString(), "$options": "i" };
+        }
+        if (req.query.abilities) {
+            paramsObject.abilities = { "$regex": JSON.parse(req.query.abilities).toString(), "$options": "i" };
+        }
+
+        Char.find(paramsObject, function (err, chars) {
+
+            console.log(req.query);
+            if (err) {
+                res.status(404).send(err);
+            };
+            console.log('getting the characters by search results.');
+            res.send(chars);
+        });
+    });
+
+    
+    app.post('/api/chars/create', (req, res) => {
+        
+        Char.create({
+            imageURL: (req.body.imageURL ? req.body.imageURL : ""), 
+            company: (req.body.company ? req.body.company : "") , 
+            name: (req.body.name ? req.body.name : ""),
+            civName: (req.body.civName ? req.body.civName : ""),
+            alignment: (req.body.alignment ? req.body.alignment : ""),
+            firstDebut: (req.body.firstDebut ? req.body.firstDebut : ""),
+            abilities: (req.body.abilities ? req.body.abilities : []),
+            teams: (req.body.teams ? req.body.teams : []),
+            desc: (req.body.desc ? req.body.desc : "")}, function (err, char) {
+                if (err) {
+                    res.status(500).send(err) 
+                };
+                console.log('trying to make new character document.');
+                res.send(char);
+            });
+        
+    });
+
+    const requestPassByUserName = (userName) => {
+        // CALL THE DB AND REQUEST THE PASSWORD ASOCIATED WITH THIS USERNAME
+        return new Promise((resolve, reject) => {
+            resolve( "safePassword")
+        })
+    }
+
+    app.get('/api/login', (req, res) => {
+
+        // res.send('Helo')
+        console.log("Body: ", req.body)
+
+        let { username, password } = req.body
+
+            console.log("DV: ", username, password)
+            // res.send({  status: 'success' })
+
+            requestPassByUserName(username)
+            .then((resp) => {
+                if(resp === password ){
+                    res.send({  status: 'success' })
+                }else{
+                    res.send({  status: 'fail' })
+                }
+            }).catch((err) => {
+                console.log("Error while login: ", err)
+                res.send({  status: 'fail' })
+            })
+
+        
+    });
+
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+
+app.listen(port, () => console.log(`This API is listening on port ${port}!`));
+
+
+
+
+
+
+
+
+
+//Deliver the 
+
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// // the __dirname is the current directory from where the script is running
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/ping', function (req, res) {
+//  return res.send('pong');
+// });
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+
+
+//app.listen(port);
